d0061eee8dc3cc3aeaadbe08146e2a837cd97644
Arin  Dixit <id2013663@gmail.com>
2026-02-09 10:41:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://Localhost:3002', // Your backend server
+        target: 'https://collaborate-1.onrender.com', // Your backend server
         changeOrigin: true,
       },
     },
