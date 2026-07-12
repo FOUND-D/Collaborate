63960635e3bccbc71bb1783ebb9c7bf6f9463901
Bhavya Agarwal <2401730012@krmu.edu.in>
2026-07-12 20:45:59 +0530

    refactor: update ThemeToggle.jsx

--- client/src/components/ThemeToggle.jsx
+++ client/src/components/ThemeToggle.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useTheme } from '../context/ThemeContext';
+import './ThemeToggle.css';
 
-const ThemeToggle = () => {
+const ThemeToggle = ({ collapsed = false }) => {
   return null;
 };
 
